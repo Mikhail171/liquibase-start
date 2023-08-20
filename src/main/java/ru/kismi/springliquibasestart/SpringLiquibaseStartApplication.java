3f9de0465a116dc1cb7ecb63fbdb3c779028e6e4
mihailkiselev <kismichel17@gmail.com>
2023-08-20 15:48:15 +0300

    liquibase scripts

---
+++ src/main/java/ru/kismi/springliquibasestart/SpringLiquibaseStartApplication.java
@@ -0,0 +1,13 @@
+package ru.kismi.springliquibasestart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLiquibaseStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLiquibaseStartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/kismi/springliquibasestart/SpringLiquibaseStartApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kismi.springliquibasestart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLiquibaseStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
